f5a725fba71679c9460797ec4a75ce7233efdf53
Rama-Shannis <112776312+Ramashannis@users.noreply.github.com>
2024-09-09 20:40:07 -0400

    creation of the 4 types of tile

--- client/src/app/pages/game-editor-page/game-editor-page.component.ts
+++ client/src/app/pages/game-editor-page/game-editor-page.component.ts
@@ -1,19 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-game-editor-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-editor-page.component.html',
   styleUrl: './game-editor-page.component.scss'
 })
 export class GameEditorPageComponent {
+  grid: number[] = []; 
+  selectedTool: string = 'base'; 
+  selectTool(tool: string): void {
+    this.selectedTool = tool; 
+  }
   game = { name: '', description: '' };
 
+  saveGame(): void {
+    console.log('Game saved:', this.game);
+  }
+  resetGame(): void {
+    this.game.name = '';
+    this.game.description = '';
+    console.log('Game reset to default');
+  }
   updateGameName(event: any): void {
     this.game.name = event.target.value;
   }
-
   updateGameDescription(event: any): void {
     this.game.description = event.target.value;
   }
