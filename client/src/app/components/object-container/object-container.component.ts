b3fd649515b6bd8731bc2f5d87d6aa14493fbc56
NoelaPanier <noela.panier@polymtl.ca>
2024-09-15 20:45:55 -0400

    add the numbers of items and starting points according to the grid

--- client/src/app/components/object-container/object-container.component.ts
+++ client/src/app/components/object-container/object-container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GridSize } from '../../classes/grid-size.enum';
 
 @Component({
     selector: 'app-object-container',
@@ -6,10 +7,27 @@ import { Component } from '@angular/core';
     templateUrl: './object-container.component.html',
     styleUrls: ['./object-container.component.scss'],
 })
-export class ObjectContainerComponent {
+export class ObjectContainerComponent implements OnInit {
     objects = [
         { name: 'Bouclier', img: '../../assets/objects/Shield.png' },
         { name: 'Object 2', img: '../../../assets/objects/Shield.png' },
         { name: 'Object 3', img: '../../../assets/objects/Shield.png' },
     ];
+
+    gridSize: GridSize = GridSize.Medium; // for test
+    displayedNumber: number;
+
+    ngOnInit() {
+        this.displayedNumber = this.getNumberByGridSize(this.gridSize);
+    }
+
+    getNumberByGridSize(size: GridSize): number {
+        if (size == GridSize.Small) {
+            return 2;
+        } else if (size == GridSize.Medium) {
+            return 4;
+        } else if (size == GridSize.Large) {
+            return 6;
+        } else return 0;
+    }
 }
