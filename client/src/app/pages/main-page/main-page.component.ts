c4b90e0bfe3e382292a68b8c4c820573795f8cdb
AliEl-Ak <ali-j-ak@outlook.com>
2024-09-18 11:14:47 -0400

    push client small problem

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -10,39 +10,6 @@ export class MainPageComponent {
 
     readonly teamNumber: string = 'Équipe 213';
     readonly teamMembers: string[] = ['Sobhi Sandakli', 'Rama Shannis', 'Noëla Panier', 'Mouneïssa Cisse', 'Ali El-Akhras'];
-
-<<<<<<< HEAD
-    sendTimeToServer(): void {
-        const newTimeMessage: Message = {
-            title: 'Hello from the client',
-            body: 'Time is : ' + new Date().toString(),
-        };
-        // Important de ne pas oublier "subscribe" ou l'appel ne sera jamais lancé puisque personne l'observe
-        this.communicationService.basicPost(newTimeMessage).subscribe({
-            next: (response) => {
-                const responseString = `Le serveur a reçu la requête a retourné un code ${response.status} : ${response.statusText}`;
-                this.message.next(responseString);
-            },
-            error: (err: HttpErrorResponse) => {
-                const responseString = `Le serveur ne répond pas et a retourné : ${err.message}`;
-                this.message.next(responseString);
-            },
-        });
-    }
-
-    getMessagesFromServer(): void {
-        this.communicationService
-            .basicGet()
-            // Cette étape transforme l'objet Message en un seul string
-            .pipe(
-                map((message: Message) => {
-                    return `${message.title} ${message.body}`;
-                }),
-            )
-            .subscribe(this.message);
-    }
-=======
     // Disable the "Join a game" button for Sprint 1
     isJoinDisabled: boolean = true;
->>>>>>> e1be7a9fb8e81f7e0488c0185330079176759521
 }
