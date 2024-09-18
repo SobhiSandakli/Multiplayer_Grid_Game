0ffa6ac854a9d2d3d116195d83da353fa313e247
NoelaPanier <noela.panier@polymtl.ca>
2024-09-18 18:54:46 -0400

    review code

--- client/src/app/pages/game-editor-page/game-editor-page.component.spec.ts
+++ client/src/app/pages/game-editor-page/game-editor-page.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GameEditorPageComponent } from './game-editor-page.component';
 
 describe('GameEditorPageComponent', () => {
