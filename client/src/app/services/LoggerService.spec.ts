2e6f15ca46965f054bd58058817a1b5a2dbff0ee
AliEl-Ak <ali-j-ak@outlook.com>
2024-09-18 19:13:28 -0400

    Made sure every file i modified have 100% coverage and the tests works

---
+++ client/src/app/services/LoggerService.spec.ts
@@ -0,0 +1,38 @@
+import { TestBed } from '@angular/core/testing';
+import { LoggerService } from './LoggerService';
+
+describe('LoggerService', () => {
+  let service: LoggerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LoggerService);
+  });
+
+  it('should log a message using console.log', () => {
+    spyOn(console, 'log');
+    const message = 'This is a log message';
+
+    service.log(message);
+
+    expect(console.log).toHaveBeenCalledWith(message);
+  });
+
+  it('should log an error using console.error', () => {
+    spyOn(console, 'error');
+    const errorMessage = 'This is an error message';
+
+    service.error(errorMessage);
+
+    expect(console.error).toHaveBeenCalledWith(errorMessage);
+  });
+
+  it('should log a warning using console.warn', () => {
+    spyOn(console, 'warn');
+    const warnMessage = 'This is a warning message';
+
+    service.warn(warnMessage);
+
+    expect(console.warn).toHaveBeenCalledWith(warnMessage);
+  });
+});
