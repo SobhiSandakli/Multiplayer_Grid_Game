dae4754da864b383767ba024f276c33ad9372c4f
NoelaPanier <noela.panier@polymtl.ca>
2024-09-18 11:48:34 -0400

    resolve merge request

--- client/src/app/pages/game-editor-page/game-editor-page.component.ts
+++ client/src/app/pages/game-editor-page/game-editor-page.component.ts
@@ -1,27 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { GridComponent } from '@app/components/grid/grid.component';
+import { ObjectContainerComponent } from '@app/components/object-container/object-container.component';
 
 @Component({
-  selector: 'app-game-editor-page',
-  standalone: true,
-  imports: [CommonModule, GridComponent],
-  templateUrl: './game-editor-page.component.html',
-  styleUrl: './game-editor-page.component.scss',
+    selector: 'app-game-editor-page',
+    standalone: true,
+    imports: [CommonModule, GridComponent, ObjectContainerComponent],
+    templateUrl: './game-editor-page.component.html',
+    styleUrl: './game-editor-page.component.scss',
 })
-export class GameEditorPageComponent implements OnInit {
-    rows: number = 4; // Nombre de lignes
-    cols: number = 4; // Nombre de colonnes
-    grid: number[] = []; // Tableau pour les cellules de la grille
-
 export class GameEditorPageComponent {
-  
-  selectedTool: string = 'base'; 
-  selectTool(tool: string): void {
-    this.selectedTool = tool; 
-  }
-  
-  game = { name: '', description: '' };
+    selectedTool: string = 'base';
+    selectTool(tool: string): void {
+        this.selectedTool = tool;
+    }
 
     game = { name: '', description: '' };
 
