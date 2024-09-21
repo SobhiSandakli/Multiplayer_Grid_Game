8e701f73efa47b0b1d43e7a63f1b863d389cecd9
NoelaPanier <noela.panier@polymtl.ca>
2024-09-21 18:13:22 -0400

    review grid service

---
+++ client/src/app/services/grid.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class GridService {
+    // Liste des tuiles de la grille partagée
+    private gridTiles: { images: string[] }[][] = [];
+
+    // Générer une grille avec le nombre spécifié de tuiles
+    generateGrid(size: number, defaultImage: string) {
+        this.gridTiles = Array.from({ length: size }, () => Array.from({ length: size }, () => ({ images: [defaultImage] })));
+    }
+
+    // Récupérer la liste des tuiles de la grille
+    getGridTiles(): { images: string[] }[][] {
+        return this.gridTiles;
+    }
+
+    // Ajouter une image à une tuile spécifique
+    addImageToTile(rowIndex: number, colIndex: number, imageLink: string) {
+        if (this.gridTiles[rowIndex] && this.gridTiles[rowIndex][colIndex]) {
+            this.gridTiles[rowIndex][colIndex].images.push(imageLink);
+        }
+    }
+}
