144427f5d41c1ddc07b062b6673a51f65ef09d04
NoelaPanier <noela.panier@polymtl.ca>
2024-09-23 10:54:50 -0400

    change html object container and component

--- client/src/app/components/object-container/object-container.component.ts
+++ client/src/app/components/object-container/object-container.component.ts
@@ -1,3 +1,5 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { GridSize } from '../../classes/grid-size.enum';
 
@@ -6,11 +8,40 @@ import { GridSize } from '../../classes/grid-size.enum';
     standalone: true,
     templateUrl: './object-container.component.html',
     styleUrls: ['./object-container.component.scss'],
+    imports: [CommonModule, DragDropModule],
 })
 export class ObjectContainerComponent implements OnInit {
     gridSize: GridSize = GridSize.Large; // for test
     displayedNumber: number;
+    objectsList = [
+        { description: 'Le bouclier: +2 points en défense.', link: '../../../assets/objects/Shield.png' },
+        { description: 'La potion critique: +2 points sur la vie et -1 point en attaque.', link: '../../../assets/objects/Critical-Potion.png' },
+        { description: "La clé: ouvrir ou fermer une porte peu importe l'endroit où tu te trouves.", link: '../../../assets/objects/Key.png' },
+    ];
+    elementCoordinates: { top: number; left: number; right: number; bottom: number; width: number; height: number } | null = null;
 
+    getCoordinates(element: HTMLElement) {
+        const rect = element.getBoundingClientRect();
+        this.elementCoordinates = {
+            top: rect.top,
+            left: rect.left,
+            right: rect.right,
+            bottom: rect.bottom,
+            width: rect.width,
+            height: rect.height,
+        };
+    }
+
+    onRightClick(element: HTMLElement, event: MouseEvent) {
+        event.preventDefault(); // Empêche le menu contextuel de s'afficher
+        console.log(element);
+        if (this.elementCoordinates) {
+            element.style.position = 'relative'; // Assure-toi que l'élément peut être repositionné
+            element.style.top = `${this.elementCoordinates.top}px`; // Ajuste pour le défilement de la page
+            element.style.left = `${this.elementCoordinates.left}px`;
+            element.style.right = `${this.elementCoordinates.right}px`; // Ajuste pour le défilement de la page
+        }
+    }
     ngOnInit() {
         this.displayedNumber = this.getNumberByGridSize(this.gridSize);
     }
