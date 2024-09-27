6b41e275c853ff62648e2e91aebc9bce6a345b60
AliEl-Ak <ali-j-ak@outlook.com>
2024-09-27 11:00:27 -0400

    fix:fixing linting problem

--- client/src/app/components/button-component/button.component.ts
+++ client/src/app/components/button-component/button.component.ts
@@ -1,23 +1,20 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Router } from '@angular/router';
-
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+    selector: 'app-button',
+    templateUrl: './button.component.html',
+    styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
-  constructor(router:Router){};
-  // Inputs for customizations
-  @Input() label: string = ''; // Button text
-  @Input() icon: any; // Icon input
-  @Input() routerLink: string | string[] = ''; // Router link input
+    // Inputs for customizations
+    @Input() label: string = ''; // Button text
+    @Input() icon: unknown; // Icon input
+    @Input() routerLink: string | string[] = ''; // Router link input
 
-  // Output event to handle button click
-  @Output() buttonClick = new EventEmitter<void>();
+    // Output event to handle button click
+    @Output() buttonClick = new EventEmitter<void>();
 
-  // Method to emit the click event
-  onClick(): void {
-    this.buttonClick.emit();
-  }
+    // Method to emit the click event
+    onClick(): void {
+        this.buttonClick.emit();
+    }
 }
