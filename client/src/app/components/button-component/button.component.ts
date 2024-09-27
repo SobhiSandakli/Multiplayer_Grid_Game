fb357f3976e204df30a1be31e97cd66169882f0e
AliEl-Ak <ali-j-ak@outlook.com>
2024-09-27 11:16:59 -0400

    fix:fixing lint

--- client/src/app/components/button-component/button.component.ts
+++ client/src/app/components/button-component/button.component.ts
@@ -6,9 +6,9 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core';
     styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
-    @Input() label: string = ''; 
+    @Input() label: string = '';
     @Input() icon: IconProp;
-    @Input() routerLink: string | string[] = ''; 
+    @Input() routerLink: string | string[] = '';
     @Output() buttonClick = new EventEmitter<void>();
     onClick(): void {
         this.buttonClick.emit();
