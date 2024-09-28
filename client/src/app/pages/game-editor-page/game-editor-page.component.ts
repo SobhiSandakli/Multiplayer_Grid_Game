7c94a2e45867268187131cd7e7672ddf4ca26cd5
SobhiSandakli <sobhi.sandakli@gmail.com>
2024-09-28 13:35:27 -0400

    Wrote the tests for validateGame

--- client/src/app/pages/game-editor-page/game-editor-page.component.ts
+++ client/src/app/pages/game-editor-page/game-editor-page.component.ts
@@ -7,41 +7,41 @@ import { ObjectContainerComponent } from '@app/components/object-container/objec
 import { TileComponent } from '@app/components/tile/tile.component';
 
 @Component({
-  selector: 'app-game-editor-page',
-  standalone: true,
-  imports: [CommonModule, GridComponent, ObjectContainerComponent, TileComponent],
-  templateUrl: './game-editor-page.component.html',
-  styleUrls: ['./game-editor-page.component.scss'],
+    selector: 'app-game-editor-page',
+    standalone: true,
+    imports: [CommonModule, GridComponent, ObjectContainerComponent, TileComponent],
+    templateUrl: './game-editor-page.component.html',
+    styleUrls: ['./game-editor-page.component.scss'],
 })
 export class GameEditorPageComponent {
-  isNameExceeded = false;
-  isDescriptionExceeded = false;
+    isNameExceeded = false;
+    isDescriptionExceeded = false;
     readonly maxLengthName: number = 30;
     readonly maxLengthDescription: number = 200;
 
-  constructor(private validateGameService: ValidateGameService, private gridService: GridService) {}
+    constructor(
+        private validateGameService: ValidateGameService,
+        private gridService: GridService,
+    ) {}
 
-  onNameInput(event: Event): void {
-    const textarea = event.target as HTMLTextAreaElement;
-    this.isNameExceeded = textarea.value.length > this.maxLengthName;
-  }
-
-  onDescriptionInput(event: Event): void {
-    const textarea = event.target as HTMLTextAreaElement;
-    this.isDescriptionExceeded = textarea.value.length > this.maxLengthDescription;
-  }
-
-  // Method called when the "Sauvegarder" button is clicked
-  onSave(): void {
-    const gridArray = this.gridService.getGridTiles(); // Get the grid data from GridService
-    console.log(gridArray);
-    // Validate all conditions using the ValidateGameService
-    if (this.validateGameService.validateAll(gridArray)) {
-      console.log('Validation successful! Saving the game...');
-      // Logic to save the game (e.g., send the data to the backend)
-    } else {
-      console.log('Validation failed! Please check the game configuration.');
-      // Show error message to the user
+    onNameInput(event: Event): void {
+        const textarea = event.target as HTMLTextAreaElement;
+        this.isNameExceeded = textarea.value.length > this.maxLengthName;
     }
-  }
+
+    onDescriptionInput(event: Event): void {
+        const textarea = event.target as HTMLTextAreaElement;
+        this.isDescriptionExceeded = textarea.value.length > this.maxLengthDescription;
+    }
+
+    // Method called when the "Sauvegarder" button is clicked
+    onSave(): void {
+        const gridArray = this.gridService.getGridTiles(); // Get the grid data from GridService
+        // Validate all conditions using the ValidateGameService
+        if (this.validateGameService.validateAll(gridArray)) {
+            // Logic to save the game (e.g., send the data to the backend)
+        } else {
+            // Handle validation errors
+        }
+    }
 }
--- client/src/app/services/validateGame.service.spec.ts
+++ client/src/app/services/validateGame.service.spec.ts
@@ -17,96 +17,159 @@ describe('ValidateGameService', () => {
 
     it('should return true and log success when all terrain tiles are accessible', () => {
         const grid = [
-            [{ images: ['assets/grass.png'] }, { images: ['assets/grass.png'] }],
-            [{ images: ['assets/grass.png'] }, { images: ['assets/grass.png'] }],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+            ],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+            ],
         ];
         const visited = [
             [true, true],
             [true, true],
         ];
-        expect(service.verifyAllTerrainTiles(grid, visited, 2, 2)).toBeTrue();
-        expect(loggerSpy.log).toHaveBeenCalledWith('All terrain tiles are accessible.');
+        const result = service.verifyAllTerrainTiles(grid, visited, 2, 2);
+        expect(result).toEqual({ valid: true, errors: [] });
     });
 
     it('should return false and log an error when some terrain tiles are not accessible', () => {
         const grid = [
-            [{ images: ['assets/grass.png'] }, { images: ['assets/tiles/Wall.png'] }],
-            [{ images: ['assets/tiles/Wall.png'] }, { images: ['assets/tiles/Wall.png'] }],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/tiles/Wall.png'], isOccuped: false },
+            ],
+            [
+                { images: ['assets/tiles/Wall.png'], isOccuped: false },
+                { images: ['assets/tiles/Wall.png'], isOccuped: false },
+            ],
         ];
         const visited = [
             [false, true],
             [true, true],
         ];
-        expect(service.verifyAllTerrainTiles(grid, visited, 2, 2)).toBeFalse();
-        expect(loggerSpy.error).toHaveBeenCalledWith('Terrain tile at row: 1, col: 1 is not accessible.');
+        const result = service.verifyAllTerrainTiles(grid, visited, 2, 2);
+        expect(result).toEqual({ valid: false, errors: ["La tuile de terrain à la ligne : 1, col: 1 n'est pas accessible."] });
+        expect(loggerSpy.error).toHaveBeenCalledWith("La tuile de terrain à la ligne : 1, col: 1 n'est pas accessible.");
     });
 
     // Test surface area validation
     it('should return true if more than 50% of the grid is terrain', () => {
         const grid = [
-            [{ images: ['assets/grass.png'] }, { images: ['assets/grass.png'] }],
-            [{ images: ['assets/grass.png'] }, { images: ['some_other_image.png'] }],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+            ],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['some_other_image.png'], isOccuped: false },
+            ],
         ];
         expect(service.isSurfaceAreaValid(grid)).toBeTrue();
     });
-
     it('should return false if less than 50% of the grid is terrain', () => {
         const grid = [
-            [{ images: ['assets/grass.png'] }, { images: ['some_other_image.png'] }],
-            [{ images: ['some_other_image.png'] }, { images: ['some_other_image.png'] }],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['some_other_image.png'], isOccuped: false },
+            ],
+            [
+                { images: ['some_other_image.png'], isOccuped: false },
+                { images: ['some_other_image.png'], isOccuped: false },
+            ],
         ];
         expect(service.isSurfaceAreaValid(grid)).toBeFalse();
     });
-
     // Test terrain tiles accessibility
     it('should return false if no start point is found', () => {
         const grid = [
-            [{ images: [] }, { images: [] }],
-            [{ images: [] }, { images: [] }],
+            [
+                { images: [], isOccuped: false },
+                { images: [], isOccuped: false },
+            ],
+            [
+                { images: [], isOccuped: false },
+                { images: [], isOccuped: false },
+            ],
         ];
-        expect(service.areAllTerrainTilesAccessible(grid)).toBeFalse();
+        const result = service.areAllTerrainTilesAccessible(grid);
+        expect(result).toEqual({ valid: true, errors: [] });
     });
 
     it('should return true if all terrain tiles are accessible from start point', () => {
         const grid = [
-            [{ images: ['../../../assets/objects/started-points.png'] }, { images: ['assets/grass.png'] }],
-            [{ images: ['assets/grass.png'] }, { images: ['assets/grass.png'] }],
+            [
+                { images: ['../../../assets/objects/started-points.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+            ],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+            ],
         ];
-        expect(service.areAllTerrainTilesAccessible(grid)).toBeTrue();
+        const result = service.areAllTerrainTilesAccessible(grid);
+        expect(result).toEqual({ valid: true, errors: [] });
     });
 
     // Test door placement
     it('should return false if doors are not correctly placed between walls', () => {
         const grid = [
-            [{ images: ['assets/tiles/Door.png'] }, { images: ['assets/grass.png'] }],
-            [{ images: ['assets/grass.png'] }, { images: ['assets/tiles/Wall.png'] }],
+            [
+                { images: ['assets/tiles/Door.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+            ],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/tiles/Wall.png'], isOccuped: false },
+            ],
         ];
-        expect(service.areDoorsCorrectlyPlaced(grid)).toBeFalse();
+        const result = service.areDoorsCorrectlyPlaced(grid);
+        expect(result).toEqual({ valid: false, errors: ["La porte à la ligne: 1, col: 1 n'est pas bien placée."] });
     });
 
     it('should return true if doors are correctly placed between walls', () => {
         const grid = [
-            [{ images: ['assets/grass.png'] }, { images: ['assets/grass.png'] }, { images: ['assets/grass.png'] }],
-            [{ images: ['assets/tiles/Wall.png'] }, { images: ['assets/tiles/Door.png'] }, { images: ['assets/tiles/Wall.png'] }],
-            [{ images: ['assets/grass.png'] }, { images: ['assets/grass.png'] }, { images: ['assets/grass.png'] }],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+            ],
+            [
+                { images: ['assets/tiles/Wall.png'], isOccuped: false },
+                { images: ['assets/tiles/Door.png'], isOccuped: false },
+                { images: ['assets/tiles/Wall.png'], isOccuped: false },
+            ],
+            [
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+                { images: ['assets/grass.png'], isOccuped: false },
+            ],
         ];
-        expect(service.areDoorsCorrectlyPlaced(grid)).toBeTrue();
+        const result = service.areDoorsCorrectlyPlaced(grid);
+        expect(result).toEqual({ valid: true, errors: [] });
     });
 
     // Test case for validating start points correctly based on grid size
     it('should validate start points correctly based on grid size', () => {
-        const grid10 = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => ({ images: [] as string[] })));
+        const grid10: { images: string[]; isOccuped: boolean }[][] = Array.from({ length: 10 }, () =>
+            Array.from({ length: 10 }, () => ({ images: [] as string[], isOccuped: false })),
+        );
         grid10[0][0].images.push('../../../assets/objects/started-points.png');
         grid10[0][1].images.push('../../../assets/objects/started-points.png');
         expect(service.areStartPointsCorrect(grid10)).toBeTrue();
 
-        const grid10_2 = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => ({ images: [] as string[] })));
-        grid10_2[0][0].images.push('../../../assets/objects/started-points.png'); // Only 1 start point added
-        expect(service.areStartPointsCorrect(grid10_2)).toBeFalse();
-        expect(loggerSpy.error).toHaveBeenCalledWith('Start points validation failed: Expected 2 start points, but found 1.');
+        const grid102: { images: string[]; isOccuped: boolean }[][] = Array.from({ length: 10 }, () =>
+            Array.from({ length: 10 }, () => ({ images: [] as string[], isOccuped: false })),
+        );
+        grid102[0][0].images.push('../../../assets/objects/started-points.png'); // Only 1 start point added
+        expect(service.areStartPointsCorrect(grid102)).toBeFalse();
+        expect(loggerSpy.error).toHaveBeenCalledWith('La validation des points de départ a échoué : 2 points de départ attendus, mais 1 trouvés.');
 
         // Create a grid of size 15 with exactly 4 start points
-        const grid15 = Array.from({ length: 15 }, () => Array.from({ length: 15 }, () => ({ images: [] as string[] }))); // Explicitly declare as string[]
+        const grid15: { images: string[]; isOccuped: boolean }[][] = Array.from({ length: 15 }, () =>
+            Array.from({ length: 15 }, () => ({ images: [] as string[], isOccuped: false })),
+        );
         grid15[0][0].images.push('../../../assets/objects/started-points.png');
         grid15[0][1].images.push('../../../assets/objects/started-points.png');
         grid15[0][2].images.push('../../../assets/objects/started-points.png');
@@ -114,7 +177,9 @@ describe('ValidateGameService', () => {
         expect(service.areStartPointsCorrect(grid15)).toBeTrue();
 
         // Create a grid of size 20 with exactly 6 start points
-        const grid20 = Array.from({ length: 20 }, () => Array.from({ length: 20 }, () => ({ images: [] as string[] }))); // Explicitly declare as string[]
+        const grid20: { images: string[]; isOccuped: boolean }[][] = Array.from({ length: 20 }, () =>
+            Array.from({ length: 20 }, () => ({ images: [] as string[], isOccuped: false })),
+        );
         grid20[0][0].images.push('../../../assets/objects/started-points.png');
         grid20[0][1].images.push('../../../assets/objects/started-points.png');
         grid20[0][2].images.push('../../../assets/objects/started-points.png');
@@ -123,34 +188,73 @@ describe('ValidateGameService', () => {
         grid20[0][5].images.push('../../../assets/objects/started-points.png');
         expect(service.areStartPointsCorrect(grid20)).toBeTrue();
 
-        const grid8 = Array.from({ length: 8 }, () => Array.from({ length: 8 }, () => ({ images: [] as string[] })));
+        const grid8: { images: string[]; isOccuped: boolean }[][] = Array.from({ length: 8 }, () =>
+            Array.from({ length: 8 }, () => ({ images: [] as string[], isOccuped: false })),
+        );
         grid8[0][0].images.push('../../../assets/objects/started-points.png');
         grid8[0][1].images.push('../../../assets/objects/started-points.png');
         expect(service.areStartPointsCorrect(grid8)).toBeTrue();
     });
 
-    // Test overall validation
+    // Test overall validation when all conditions are true
     it('should return true and log success when all validation methods return true', () => {
+        const gridSize = 10;
         spyOn(service, 'isSurfaceAreaValid').and.returnValue(true);
-        spyOn(service, 'areAllTerrainTilesAccessible').and.returnValue(true);
-        spyOn(service, 'areDoorsCorrectlyPlaced').and.returnValue(true);
+        spyOn(service, 'areAllTerrainTilesAccessible').and.returnValue({ valid: true, errors: [] });
+        spyOn(service, 'areDoorsCorrectlyPlaced').and.returnValue({ valid: true, errors: [] });
         spyOn(service, 'areStartPointsCorrect').and.returnValue(true);
 
-        const grid = Array(10).fill(Array(10).fill({ images: ['assets/grass.png'] }));
+        const grid = Array(gridSize).fill(Array(gridSize).fill({ images: ['assets/grass.png'] }));
         expect(service.validateAll(grid)).toBeTrue();
-        expect(loggerSpy.log).toHaveBeenCalledWith('Game validation successful. All checks passed.');
+        expect(loggerSpy.log).toHaveBeenCalledWith('Validation du jeu réussie. Toutes les vérifications ont été passées.');
         expect(loggerSpy.error).not.toHaveBeenCalled();
     });
 
+    // Test overall validation when any condition is false
     it('should return false and log an error when any validation method returns false', () => {
+        const gridSize = 10;
         spyOn(service, 'isSurfaceAreaValid').and.returnValue(true);
-        spyOn(service, 'areAllTerrainTilesAccessible').and.returnValue(false); // One false makes all false
-        spyOn(service, 'areDoorsCorrectlyPlaced').and.returnValue(true);
+        spyOn(service, 'areAllTerrainTilesAccessible').and.returnValue({ valid: false, errors: ['Some terrain tiles are not accessible.'] });
+        spyOn(service, 'areDoorsCorrectlyPlaced').and.returnValue({ valid: true, errors: [] });
         spyOn(service, 'areStartPointsCorrect').and.returnValue(true);
 
-        const grid = Array(10).fill(Array(10).fill({ images: ['assets/grass.png'] }));
+        const grid = Array(gridSize).fill(Array(gridSize).fill({ images: ['assets/grass.png'] }));
         expect(service.validateAll(grid)).toBeFalse();
-        expect(loggerSpy.error).toHaveBeenCalledWith('Game validation failed. Please review the errors above.');
+        expect(loggerSpy.error).toHaveBeenCalledWith('Échec de la validation du jeu.\n• Some terrain tiles are not accessible.\n');
         expect(loggerSpy.log).not.toHaveBeenCalled();
+    });
+
+    // Test overall validation when any condition is false
+    it('should return false and log an error when any validation method returns false', () => {
+        const gridSize = 10;
+        spyOn(service, 'isSurfaceAreaValid').and.returnValue(true);
+        spyOn(service, 'areAllTerrainTilesAccessible').and.returnValue({ valid: true, errors: [] });
+        spyOn(service, 'areDoorsCorrectlyPlaced').and.returnValue({ valid: false, errors: [] });
+        spyOn(service, 'areStartPointsCorrect').and.returnValue(true);
+
+        const grid = Array(gridSize).fill(Array(gridSize).fill({ images: ['assets/grass.png'] }));
+        expect(service.validateAll(grid)).toBeFalse();
+        expect(loggerSpy.error).toHaveBeenCalledWith(
+            'Échec de la validation du jeu.\n' + '• Problèmes de placement des portes :\n' + '  - \n', // Ensure spaces and newline characters are correctly placed
+        );
+        expect(loggerSpy.log).not.toHaveBeenCalled();
+    });
+
+    // Test to see if a tile is terrain
+    it('should return false when the cell does not contain grass image', () => {
+        const grid = [
+            [
+                { images: ['assets/tiles/Door.png'], isOccuped: false },
+                { images: ['assets/tiles/Door.png'], isOccuped: false },
+            ],
+            [
+                { images: ['assets/tiles/Door.png'], isOccuped: false },
+                { images: ['assets/tiles/Door.png'], isOccuped: false },
+            ],
+        ];
+
+        const row = 0;
+        const col = 1;
+        expect(service.isTerrain(grid, row, col)).toBeFalse();
     });
 });
