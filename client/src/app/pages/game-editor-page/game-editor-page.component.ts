dadbf5c9601c9620d298cd2a4b0e939c17385d4a
NoelaPanier <noela.panier@polymtl.ca>
2024-09-28 13:35:27 -0400

    add modal

--- client/src/app/pages/game-editor-page/game-editor-page.component.ts
+++ client/src/app/pages/game-editor-page/game-editor-page.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
+import { ConfirmationPopupComponent } from '@app/components/confirmation-popup/confirmation-popup.component';
 import { GridComponent } from '@app/components/grid/grid.component';
 import { ObjectContainerComponent } from '@app/components/object-container/object-container.component';
 import { TileComponent } from '@app/components/tile/tile.component';
 import { GridService } from '@app/services/grid.service';
-
 @Component({
     selector: 'app-game-editor-page',
     standalone: true,
-    imports: [CommonModule, GridComponent, ObjectContainerComponent, TileComponent],
+    imports: [CommonModule, GridComponent, ObjectContainerComponent, TileComponent, ConfirmationPopupComponent],
     templateUrl: './game-editor-page.component.html',
     styleUrl: './game-editor-page.component.scss',
 })
@@ -20,6 +20,7 @@ export class GameEditorPageComponent {
     isDescriptionExceeded = false;
 
     @ViewChild(ObjectContainerComponent) objectContainer: ObjectContainerComponent;
+    showCreationPopup = false;
 
     constructor(private gridService: GridService) {}
     onNameInput(event: Event): void {
@@ -32,8 +33,20 @@ export class GameEditorPageComponent {
         this.isDescriptionExceeded = textarea.value.length > this.maxLengthDescription;
     }
 
+    confirmReset(): void {
+        this.showCreationPopup = false;
+        this.reset();
+    }
+
+    cancelReset(): void {
+        this.showCreationPopup = false;
+    }
     reset(): void {
         this.gridService.resetGrid();
         this.objectContainer.reset();
+    }
+
+    openPopup(): void {
+        this.showCreationPopup = true;
     }
 }
