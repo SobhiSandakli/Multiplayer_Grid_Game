c5bf5f489bc4ce6eae246706be7ecfc35a43c7f9
Rama-Shannis <112776312+Ramashannis@users.noreply.github.com>
2024-09-30 12:49:44 -0400

    tests tile.service

--- client/src/app/services/tile.service.spec.ts
+++ client/src/app/services/tile.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { GridService } from './grid.service';
 import { TileService } from './tile.service';
-
+interface Tile {
+    images: string[];
+    isOccuped: boolean;
+}
 describe('TileService', () => {
     let service: TileService;
+    let gridServiceSpy: jasmine.SpyObj<GridService>;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        const mockGridService = jasmine.createSpyObj('GridService', ['getGridTiles']);
+        TestBed.configureTestingModule({providers: [
+            TileService,
+            { provide: GridService, useValue: mockGridService }
+        ]
+    });
         service = TestBed.inject(TileService);
+        gridServiceSpy = TestBed.inject(GridService) as jasmine.SpyObj<GridService>;
     });
 
     it('should be created', () => {
@@ -42,5 +53,36 @@ describe('TileService', () => {
     it('should get image for ice', () => {
         const tileImage = service.getTileImage('ice');
         expect(tileImage).toBe('assets/tiles/Ice.png');
+    });
+    it('should return default image for unknown tile', () => {
+        const tileImage = service.getTileImage('unknown');
+        expect(tileImage).toBe('assets/grass.png');
+    });
+
+    it('should add object to tile', () => {
+        const mockGridTiles: Tile[][] = [
+            [{ images: [], isOccuped: false }],
+            [{ images: [], isOccuped: false }]
+        ];
+
+        gridServiceSpy.getGridTiles.and.returnValue(mockGridTiles);
+
+        service.addObjectToTile(0, 0, 'someObject.png');
+
+        expect(mockGridTiles[0][0].images).toContain('someObject.png');
+        expect(mockGridTiles[0][0].isOccuped).toBeTrue();
+    });
+
+    it('should remove object from tile', () => {
+        const mockGridTiles = [
+            [{ images: ['someObject.png'], isOccuped: true }],
+            [{ images: [], isOccuped: false }]
+        ];
+        gridServiceSpy.getGridTiles.and.returnValue(mockGridTiles);
+
+        service.removeObjectFromTile(0, 0, 'someObject.png');
+
+        expect(mockGridTiles[0][0].images).not.toContain('someObject.png');
+        expect(mockGridTiles[0][0].isOccuped).toBeFalse();
     });
 });
