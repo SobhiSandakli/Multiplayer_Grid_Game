6d3d37e21527e5b3e0b6612d8d2eb748072314e1
SobhiSandakli <sobhi.sandakli@gmail.com>
2024-10-13 19:45:15 -0400

    Trying new paths

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,14 +2,13 @@ import { provideHttpClient } from '@angular/common/http';
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { Routes, provideRouter } from '@angular/router';
+import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { AppComponent } from '@app/pages/app/app.component';
 import { CreatePageComponent } from '@app/pages/create-page/create-page.component'; 
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { WaitingViewComponent } from '@app/pages/waiting-page/waiting-page.component';
 import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
-
 import { GameEditorPageComponent } from '@app/pages/game-editor-page/game-editor-page.component';
 import { environment } from './environments/environment';
 
@@ -29,5 +28,5 @@ export const routes: Routes = [
 ];
 
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient(), provideRouter(routes), provideAnimations()],
+    providers: [provideHttpClient(), provideRouter(routes, withHashLocation()), provideAnimations()],
 });
