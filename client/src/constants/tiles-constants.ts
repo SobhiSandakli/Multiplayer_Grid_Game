670e93399dae0761935dd90e72bf74452d77eecd
AliEl-Ak <ali-j-ak@outlook.com>
2024-10-15 14:37:34 -0400

    Fix:removed the relative path so we can use absolute path so the deployed version can read the images

--- client/src/constants/tiles-constants.ts
+++ client/src/constants/tiles-constants.ts
@@ -4,25 +4,25 @@ export const TILES_LIST = [
     {
         name: 'wall',
         label: 'Mur: on ne peut pas passer à travers.',
-        imgSrc: '../../../assets/tiles/Wall.png',
+        imgSrc: 'assets/tiles/Wall.png',
         alt: 'Wall Tile',
     },
     {
         name: 'water',
         label: 'Eau',
-        imgSrc: '../../../assets/tiles/Water.png',
+        imgSrc: 'assets/tiles/Water.png',
         alt: 'Water Tile',
     },
     {
         name: 'door',
         label: 'Porte: on peut franchir une porte que si elle est ouverte.',
-        imgSrc: '../../../assets/tiles/Door.png',
+        imgSrc: 'assets/tiles/Door.png',
         alt: 'Door Tile',
     },
     {
         name: 'ice',
         label: 'Glace: on a plus de chances de tomber sur de la glace.',
-        imgSrc: '../../../assets/tiles/Ice.png',
+        imgSrc: 'assets/tiles/Ice.png',
         alt: 'Ice Tile',
     },
 ];
