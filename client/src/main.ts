dd2f6a3c3128775c36d895ff62a85c2b89f2665c
NoelaPanier <noela.panier@polymtl.ca>
2024-10-17 13:22:29 -0400

    add game page component

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,11 +5,12 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { AppComponent } from '@app/pages/app/app.component';
-import { CreatePageComponent } from '@app/pages/create-page/create-page.component'; 
-import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-import { WaitingViewComponent } from '@app/pages/waiting-page/waiting-page.component';
-import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { CreatePageComponent } from '@app/pages/create-page/create-page.component';
 import { GameEditorPageComponent } from '@app/pages/game-editor-page/game-editor-page.component';
+import { GamePageComponent } from '@app/pages/game-page/game-page.component';
+import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
+import { WaitingViewComponent } from '@app/pages/waiting-page/waiting-page.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -22,8 +23,9 @@ export const routes: Routes = [
     { path: 'material', component: MaterialPageComponent },
     { path: 'admin-page', component: AdminPageComponent },
     { path: 'create-page', component: CreatePageComponent },
-    { path: 'edit-page', component: GameEditorPageComponent},
-    {path: 'waiting', component: WaitingViewComponent},
+    { path: 'edit-page', component: GameEditorPageComponent },
+    { path: 'waiting', component: WaitingViewComponent },
+    { path: 'game', component: GamePageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
