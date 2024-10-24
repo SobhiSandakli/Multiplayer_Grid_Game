a05c27a9da8f77ef9834e83fa01379398d83237b
Rama-Shannis <112776312+Ramashannis@users.noreply.github.com>
2024-10-24 14:14:20 -0400

    modify html and scss of waiting page

--- client/src/app/pages/waiting-page/waiting-page.component.ts
+++ client/src/app/pages/waiting-page/waiting-page.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Attribute } from '@app/interfaces/attributes.interface';
+import { Game } from '@app/interfaces/game-model.interface';
 import { Player } from '@app/interfaces/player.interface';
 import { RoomLockedResponse } from '@app/interfaces/socket.interface';
+import { GameFacadeService } from '@app/services/game-facade/game-facade.service';
 import { NotificationService } from '@app/services/notification-service/notification.service';
 import { SocketService } from '@app/services/socket/socket.service';
+import { GameValidateService } from '@app/services/validate-game/gameValidate.service';
 import { faArrowLeft, faHourglassHalf, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs/internal/Subscription';
+import { MIN_PLAYERS } from 'src/constants/players-constants';
+
 @Component({
     selector: 'app-waiting-page',
     templateUrl: './waiting-page.component.html',
@@ -18,6 +23,8 @@ export class WaitingViewComponent implements OnInit, OnDestroy {
     faArrowLeft: IconDefinition = faArrowLeft;
     hourglass: IconDefinition = faHourglassHalf;
     players: Player[] = [];
+    maxPlayers: number;
+    selectedGame: Game;
     isOrganizer: boolean = false;
     popupVisible: boolean = false;
     leaveSessionPopupVisible: boolean = false;
@@ -27,17 +34,21 @@ export class WaitingViewComponent implements OnInit, OnDestroy {
     playerAvatar: string = '';
     roomLocked: boolean = false;
     playerAttributes: { [key: string]: Attribute } | undefined;
+    gameId: string | null = null;
     private readonly subscriptions: Subscription = new Subscription();
-    private gameId: string | null = null;
     constructor(
         private router: Router,
         private socketService: SocketService,
+        private gameValidateService: GameValidateService,
         private route: ActivatedRoute,
         private notificationService: NotificationService,
+        private gameFacade: GameFacadeService,
     ) {}
 
     ngOnInit(): void {
+        this.reload();
         this.initializeSessionCode();
+        this.loadGameData();
         this.subscribeToPlayerListUpdate();
         this.subscribeToExclusion();
         this.subscribeToRoomLock();
@@ -45,6 +56,7 @@ export class WaitingViewComponent implements OnInit, OnDestroy {
         this.subscribeToGameStarted();
     }
     ngOnDestroy() {
+        sessionStorage.removeItem('waitingPageReloaded');
         this.subscriptions.unsubscribe();
     }
 
@@ -68,6 +80,14 @@ export class WaitingViewComponent implements OnInit, OnDestroy {
     }
 
     startGame(): void {
+        if (!this.isNumberPlayerValid()) {
+            this.notificationService.showMessage('Le nombre de joueurs ne respecte pas les limites de la carte de jeu.');
+            return;
+        }
+        if (!this.roomLocked) {
+            this.notificationService.showMessage('La salle doit être verrouillée pour démarrer la partie.');
+            return;
+        }
         this.socketService.emitStartGame(this.sessionCode);
     }
     excludePlayer(player: Player): void {
@@ -89,14 +109,35 @@ export class WaitingViewComponent implements OnInit, OnDestroy {
         this.selectedPlayer = null;
     }
     toggleLock(): void {
-        if (this.sessionCode) {
-            this.roomLocked = !this.roomLocked;
-            this.socketService.toggleRoomLock(this.sessionCode, this.roomLocked);
+        if (this.roomLocked && this.players.length >= this.maxPlayers) {
+            if (this.isOrganizer) {
+                this.notificationService.showMessage('Vous ne pouvez pas déverrouiller la salle car le nombre maximum de joueurs est atteint.');
+            }
+            return;
         }
+        this.roomLocked = !this.roomLocked;
+        this.socketService.toggleRoomLock(this.sessionCode, this.roomLocked);
     }
     cancelExclusion(): void {
         this.popupVisible = false;
         this.selectedPlayer = null;
+    }
+    private reload(): void {
+        if (sessionStorage.getItem('waitingPageReloaded')) {
+            this.router.navigate(['/']);
+        } else {
+            sessionStorage.setItem('waitingPageReloaded', 'true');
+        }
+    }
+    private loadGame(gameId: string): void {
+        this.gameId = gameId;
+        const gameFetch = this.gameFacade.fetchGame(gameId).subscribe({
+            next: (game: Game) => {
+                this.selectedGame = game;
+                this.maxPlayers = this.gameValidateService.gridMaxPlayers(game);
+            },
+        });
+        this.subscriptions.add(gameFetch);
     }
     private subscribeToRoomLock(): void {
         this.socketService.onRoomLocked().subscribe((data: RoomLockedResponse) => {
@@ -108,6 +149,9 @@ export class WaitingViewComponent implements OnInit, OnDestroy {
             this.notificationService.showMessage(data.message);
             this.router.navigate(['/']);
         });
+    }
+    private isNumberPlayerValid(): boolean {
+        return this.players.length >= MIN_PLAYERS && this.players.length <= this.maxPlayers;
     }
     private subscribeToGameStarted(): void {
         this.socketService.onGameStarted().subscribe((data) => {
@@ -138,7 +182,6 @@ export class WaitingViewComponent implements OnInit, OnDestroy {
         this.accessCode = this.sessionCode;
         if (!this.sessionCode) {
             this.router.navigate(['/']);
-            return;
         }
     }
     private subscribeToPlayerListUpdate(): void {
@@ -151,7 +194,38 @@ export class WaitingViewComponent implements OnInit, OnDestroy {
                 this.playerAttributes = currentPlayer.attributes;
                 this.playerAvatar = currentPlayer.avatar;
             }
+            this.updatePlayersList(data.players);
+            this.updateCurrentPlayerDetails();
+            this.lockRoomIfMaxPlayersReached();
         });
+    }
+    private updatePlayersList(players: Player[]): void {
+        this.players = players;
+    }
+
+    private updateCurrentPlayerDetails(): void {
+        const currentPlayer = this.players.find((p) => p.socketId === this.socketService.getSocketId());
+        this.isOrganizer = currentPlayer ? currentPlayer.isOrganizer : false;
+        if (currentPlayer) {
+            this.playerName = currentPlayer.name;
+            this.playerAttributes = currentPlayer.attributes;
+        }
+    }
+    private lockRoomIfMaxPlayersReached(): void {
+        if (this.players.length >= this.maxPlayers) {
+            this.roomLocked = true;
+            this.socketService.toggleRoomLock(this.sessionCode, this.roomLocked);
+            if (this.isOrganizer) {
+                this.notificationService.showMessage('La salle est automatiquement verrouillée car le nombre maximum de joueurs est atteint.');
+            }
+        }
+    }
+    private loadGameData(): void {
+        if (this.gameId) {
+            this.loadGame(this.gameId);
+        } else {
+            this.router.navigate(['/']);
+        }
     }
     private subscribeToExclusion(): void {
         this.socketService.onExcluded().subscribe((data) => {
