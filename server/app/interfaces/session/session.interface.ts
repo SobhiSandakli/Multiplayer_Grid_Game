229a05359f0fd7ec9f18424e83b232cbe2ea2dfb
SobhiSandakli <sobhi.sandakli@gmail.com>
2024-11-03 17:51:57 -0500

    ajout du array combat

--- server/app/interfaces/session/session.interface.ts
+++ server/app/interfaces/session/session.interface.ts
@@ -6,11 +6,10 @@ export interface Session {
     players: Player[];
     selectedGameID: string;
     grid: { images: string[]; isOccuped: boolean }[][];
-
-    // Nouvelles propriétés pour la gestion des tours
-  turnOrder: string[];           // Liste des socketId des joueurs dans l'ordre des tours
-  currentTurnIndex: number;      // Index du joueur actuel dans turnOrder
-  currentPlayerSocketId: string; // SocketId du joueur dont c'est le tour
-  turnTimer: NodeJS.Timeout | null; // Référence au timer du tour
-  timeLeft: number;              // Temps restant en secondes pour le tour en cours
+    turnOrder: string[]; // Liste des socketId des joueurs dans l'ordre des tours
+    currentTurnIndex: number; // Index du joueur actuel dans turnOrder
+    currentPlayerSocketId: string; // SocketId du joueur dont c'est le tour
+    turnTimer: NodeJS.Timeout | null; // Référence au timer du tour
+    timeLeft: number; // Temps restant en secondes pour le tour en cours
+    combat: Player[]; // Liste des socketId des joueurs impliqués dans un combat
 }
