68ae7ae4083729f8b88dcb3084467a7ac1e2e0b7
Rama-Shannis <112776312+Ramashannis@users.noreply.github.com>
2024-11-09 15:54:43 -0500

    modify scss and add dice

--- client/src/app/components/fight/fight.component.ts
+++ client/src/app/components/fight/fight.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { DiceComponent } from '@app/components/dice/dice.component';
 import { SessionService } from '@app/services/session/session.service';
 import { SocketService } from '@app/services/socket/socket.service';
+import { Subscription } from 'rxjs';
+import { TURN_NOTIF_DURATION } from 'src/constants/game-constants';
 
 @Component({
     selector: 'app-fight',
@@ -13,27 +16,27 @@ export class FightComponent implements OnInit {
     combatOpponentInfo: {
         name: string;
         avatar: string;
-        attack: number;
-        defense: number;
-        life: number;
     } | null = null;
     isPlayerInCombat: boolean = true;
     isActive: boolean;
+    combatCurrentPlayerSocketId: string | null = null;
     @Input() isFight: boolean;
     isCombatTurn: boolean = true;
-    isAttackOptionDisabled: boolean;
-    isEvasionOptionDisabled: boolean;
+    isAttackOptionDisabled: boolean = true;
+isEvasionOptionDisabled: boolean = true;
     endGameMessage: string | null = null;
     winnerName: string | null = null;
     isFightActive: boolean = true;
     opposentPlayer: string;
-
+    private subscriptions: Subscription = new Subscription();
     attackBase: number;
     defenceBase: number;
     attackRoll: number;
     defenceRoll: number;
     attackSuccess: boolean;
     escapeAttempt: number = 2;
+    isCombatInProgress: boolean;
+    action: number;
 
     get sessionCode() {
         return this.sessionService.sessionCode;
@@ -48,26 +51,116 @@ export class FightComponent implements OnInit {
     constructor(
         private socketService: SocketService,
         public sessionService: SessionService,
+        private snackBar: MatSnackBar,
     ) {}
     ngOnInit(): void {
+        this.subscriptions.add(
         this.socketService.onCombatStarted().subscribe((data) => {
-            console.log('SALUT', data.opponentAttributes);
             this.isPlayerInCombat = true;
             this.combatOpponentInfo = {
                 name: data.opponentName,
                 avatar: data.opponentAvatar,
-                attack: Number(data.opponentAttributes.attack),
-                defense: 0,
-                life: 0,
             };
-        });
-
+        })
+    )
+    this.subscriptions.add(
         this.socketService.onCombatTurnStarted().subscribe((data) => {
             this.isCombatTurn = data.playerSocketId === this.socketService.getSocketId();
             this.isAttackOptionDisabled = !this.isCombatTurn;
             this.isEvasionOptionDisabled = !this.isCombatTurn;
-        });
-    }
+        })
+    )
+    this.subscriptions.add(
+        this.socketService.onCombatNotification().subscribe((data) => {
+            if (!this.isPlayerInCombat) {
+                this.isCombatInProgress = data.combat;
+            }
+        }),
+    );
+    this.subscriptions.add(
+        this.socketService.onAttackResult().subscribe((data) => {
+            this.updateDiceResults(data.attackRoll, data.defenceRoll);
+        }),
+    );
+    this.subscriptions.add(
+        this.socketService.onCombatTurnStarted().subscribe((data) => {
+            this.isCombatTurn = data.playerSocketId === this.socketService.getSocketId();
+            this.isAttackOptionDisabled = !this.isCombatTurn;
+            this.isEvasionOptionDisabled = !this.isCombatTurn;
+            this.combatCurrentPlayerSocketId = data.playerSocketId;
+        }),
+    );
+    this.subscriptions.add(
+        this.socketService.onAttackResult().subscribe((data) => {
+            this.attackBase = data.attackBase;
+            this.attackRoll = data.attackRoll;
+            this.defenceBase = data.defenceBase;
+            this.defenceRoll = data.defenceRoll;
+            this.attackSuccess = data.success;
+            this.diceComponent.rollDice();
+            this.diceComponent.showDiceRoll(data.attackRoll, data.defenceRoll);
+        }),
+    );
+
+    this.subscriptions.add(
+        this.socketService.onEvasionResult().subscribe((data) => {
+            if (data.success) {
+                this.isFight = false;
+                this.action = 1;
+
+                this.openSnackBar('Vous avez réussi à vous échapper !');
+                this.socketService.onCombatEnded().subscribe((dataEnd) => {
+                    this.openSnackBar(dataEnd.message);
+                });
+            } else {
+                this.openSnackBar("Vous n'avez pas réussi à vous échapper.");
+            }
+        }),
+    );
+
+    this.subscriptions.add(
+        this.socketService.onDefeated().subscribe((data) => {
+            this.isCombatInProgress = false;
+            this.isPlayerInCombat = false;
+            this.isCombatTurn = false;
+            this.isFight = false;
+            this.action = 1;
+            this.combatCurrentPlayerSocketId = null;
+            this.snackBar.open(data.message, 'OK', { duration: 3000 });
+        }),
+    );
+
+    this.subscriptions.add(
+        this.socketService.onOpponentDefeated().subscribe((data) => {
+            this.isCombatInProgress = false;
+            this.isFight = false;
+            this.action = 1;
+            this.isPlayerInCombat = false;
+            this.snackBar.open(data.message, 'OK', { duration: 3000 });
+        }),
+    );
+
+    this.subscriptions.add(
+        this.socketService.onEvasionSuccess().subscribe((data) => {
+            this.isCombatInProgress = false;
+            this.isPlayerInCombat = false;
+            this.isFight = false;
+            this.action = 1;
+            this.snackBar.open(data.message, 'OK', { duration: 3000 });
+        }),
+    );
+
+    this.subscriptions.add(
+        this.socketService.onOpponentEvaded().subscribe(() => {
+            this.isPlayerInCombat = false;
+            this.isCombatInProgress = false;
+            this.isFight = false;
+            this.snackBar.open("Votre adversaire a réussi à s'échapper du combat.", 'OK', { duration: 3000 });
+        }),
+    );
+
+}
+
 
     startCombat() {
         this.socketService.emitStartCombat(this.sessionCode, this.playerAvatar, this.opposentPlayer);
@@ -101,5 +194,11 @@ export class FightComponent implements OnInit {
 
     onFightStatusChanged($event: boolean) {
         this.isFight = $event;
+    }
+    private openSnackBar(message: string, action: string = 'OK'): void {
+        this.snackBar.open(message, action, {
+            duration: TURN_NOTIF_DURATION,
+            panelClass: ['custom-snackbar'],
+        });
     }
 }
