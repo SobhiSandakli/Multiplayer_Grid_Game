ad06c2cb20a564704b6b01f7a46b9273dd8e3b18
SobhiSandakli <sobhi.sandakli@gmail.com>
2024-11-28 10:50:43 -0500

    JV distinct

--- server/app/constants/virtual-players-name.constants.ts
+++ server/app/constants/virtual-players-name.constants.ts
@@ -1,1 +1,1 @@
-export const VIRTUAL_PLAYER_NAMES = ['Jean', 'Marie', 'Pierre', 'Lucie', 'Antoine'];
+export const VIRTUAL_PLAYER_NAMES = ['Jean (JV)', 'Marie (JV)', 'Pierre (JV)', 'Lucie (JV)', 'Antoine (JV)']
