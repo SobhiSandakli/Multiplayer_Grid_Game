0d25663136f5b5d844589b3f7d401b17d000c400
SobhiSandakli <sobhi.sandakli@gmail.com>
2024-11-28 14:26:06 -0500

    fixing lint

--- server/app/constants/virtual-players-name.constants.ts
+++ server/app/constants/virtual-players-name.constants.ts
@@ -1,1 +1,1 @@
-export const VIRTUAL_PLAYER_NAMES = ['Jean (JV)', 'Marie (JV)', 'Pierre (JV)', 'Lucie (JV)', 'Antoine (JV)']
+export const VIRTUAL_PLAYER_NAMES = ['Jean (JV)', 'Marie (JV)', 'Pierre (JV)', 'Lucie (JV)', 'Antoine (JV)'];
