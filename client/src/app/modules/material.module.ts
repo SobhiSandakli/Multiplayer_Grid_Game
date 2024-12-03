59070e607c90f0e2401befe09c9f28c149fda42c
AliEl-Ak <ali-j-ak@outlook.com>
2024-12-02 20:20:15 -0500

    Last fix in projet 2

--- client/src/app/modules/material.module.ts
+++ client/src/app/modules/material.module.ts
@@ -1,42 +1,22 @@
 import { NgModule } from '@angular/core';
-// import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
-// import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
-// import { MatCheckboxModule } from '@angular/material/checkbox';
-// import { MatChipsModule } from '@angular/material/chips';
-// import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-// import { MatListModule } from '@angular/material/list';
-// import { MatMenuModule } from '@angular/material/menu';
-// import { MatPaginatorModule } from '@angular/material/paginator';
-// import { MatProgressBarModule } from '@angular/material/progress-bar';
-// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
-// import { MatSelectModule } from '@angular/material/select';
-// import { MatSidenavModule } from '@angular/material/sidenav';
-// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-// import { MatSliderModule } from '@angular/material/slider';
-// import { MatSnackBarModule } from '@angular/material/snack-bar';
-// import { MatSortModule } from '@angular/material/sort';
-// import { MatStepperModule } from '@angular/material/stepper';
-// import { MatTableModule } from '@angular/material/table';
-// import { MatTabsModule } from '@angular/material/tabs';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatListModule } from '@angular/material/list'; // For listing items, if needed
-import { MatSnackBarModule } from '@angular/material/snack-bar'; // For future notifications
+import { MatListModule } from '@angular/material/list'; 
+import { MatSnackBarModule } from '@angular/material/snack-bar'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { ButtonComponent } from '@app/components/button-component/button.component';
-// import { SnackbarComponent } from '@app/components/snackbar/snackbar.component';
 import { CharacterCreationComponent } from '@app/components/character-creation/character-creation.component';
 import { ChatComponent } from '@app/components/chat/chat.component';
 import { ConfirmationPopupComponent } from '@app/components/confirmation-popup/confirmation-popup.component';
@@ -63,35 +43,14 @@ import { StatisticsComponent } from '@app/pages/statistics-page/statistics.compo
 import { WaitingViewComponent } from '@app/pages/waiting-page/waiting-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 const modules = [
-    // MatAutocompleteModule,
     MatButtonModule,
-    // MatButtonToggleModule,
     MatCardModule,
-    // MatCheckboxModule,
-    // MatCheckboxModule,
-    // MatChipsModule,
-    // MatDatepickerModule,
     MatDialogModule,
     MatExpansionModule,
-    // MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
     MatInputModule,
-    // MatListModule,
-    // MatMenuModule,
-    // MatPaginatorModule,
-    // MatProgressBarModule,
-    // MatProgressSpinnerModule,
     MatRadioModule,
-    // MatSelectModule,
-    // MatSidenavModule,
-    // MatSliderModule,
-    // MatSlideToggleModule,
-    // MatSnackBarModule,
-    // MatSortModule,
-    // MatStepperModule,
-    // MatTableModule,
-    // MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
     MatInputModule,
@@ -107,10 +66,6 @@ const modules = [
     FormsModule,
 ];
 
-/**
- * Material module
- * IMPORTANT : IMPORT ONLY USED MODULES !!!!!!
- */
 @NgModule({
     declarations: [
         MainPageComponent,
@@ -129,7 +84,6 @@ const modules = [
         GamePageComponent,
         TimerComponent,
         InventoryPopupComponent,
-        // SnackbarComponent,
         ChatComponent,
         JoinGameComponent,
         WaitingViewComponent,
